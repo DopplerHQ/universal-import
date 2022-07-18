de70385b6b2876f8524a8b86465e0e0e1f01730d
Andre Azzolini <andre@azzolini.io>
2022-07-18 16:58:53 -0500

    Add initial project structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:import/errors"],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    eqeqeq: ["error", "always", { null: "ignore" }],
+    "import/default": "off",
+    "import/extensions": ["error", "never", { json: "always" }],
+    "import/first": "error",
+    "import/named": "off",
+    "import/namespace": "off",
+    "import/no-duplicates": "error",
+    "import/no-named-as-default-member": "off",
+    "import/no-useless-path-segments": "error",
+    "no-constant-condition": "off",
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-template": "error",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    "@typescript-eslint/no-unused-vars": ["error", { vars: "all", args: "none", ignoreRestSiblings: true }],
+  },
+  settings: {
+    "import/external-module-folders": ["node_modules"],
+    "import/resolver": {
+      typescript: {},
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx", ".d.ts"],
+    },
+  },
+};
