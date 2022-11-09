e944446624491f72f8b20f5df45be669690920e1
Maxime Caly <max.caly@doppler.com>
2022-11-08 17:43:35 -0700

    Refactor post-build.sh and remove backup files created by sed
    The -i flag for sed will create a backup file which we do not need.
    Specifying -i "" (empty quotes) leads to no backup files.
    This has the nice side effect of lowering our bundle size since we ended
    up with .js-e files which were the backup files of the sed replacements

--- core/post-build.sh
+++ core/post-build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-echo "Replacing REPLACE_POST_COMPILE_DOPPLER_UNIVERSAL_KEY_URL..."
+echo "Replacing REPLACE_POST_COMPILE_DOPPLER_UNIVERSAL_KEY_URL"
 # String manipulation escapes forward slashes with a backslash ${main_string/search_term/replace_term}
-sed -i -e "s/REPLACE_POST_COMPILE_DOPPLER_UNIVERSAL_KEY_URL/${UNIVERSAL_IMPORT_KEYS_URL//\//\\/}/" ./dist/encryption.js
+sed -i -e "s/REPLACE_POST_COMPILE_DOPPLER_UNIVERSAL_KEY_URL/${UNIVERSAL_IMPORT_KEYS_URL//\//\\/}/" ./dist/index.js
 
-echo "Replacing REPLACE_POST_COMPILE_DOPPLER_DASHBOARD_URL..."
+echo "Replacing REPLACE_POST_COMPILE_DOPPLER_DASHBOARD_URL"
 # String manipulation escapes forward slashes with a backslash ${main_string/search_term/replace_term}
 sed -i -e "s/REPLACE_POST_COMPILE_DOPPLER_DASHBOARD_URL/${DOPPLER_DASHBOARD_URL//\//\\/}/" ./dist/modal.js
 
-echo "Replacing completed!"
+PACKAGE_CORE_VERSION=$(cat package.json | jq -r .version)
+
+echo "Replacing REPLACE_CORE_PACKAGE_VERSION with $PACKAGE_CORE_VERSION"
+sed -i -e "s/REPLACE_CORE_PACKAGE_VERSION/$PACKAGE_CORE_VERSION/" ./dist/encryption.js
+sed -i -e "s/REPLACE_CORE_PACKAGE_VERSION/$PACKAGE_CORE_VERSION/" ./dist/modal.js
+
+echo "Completed replacements!"
